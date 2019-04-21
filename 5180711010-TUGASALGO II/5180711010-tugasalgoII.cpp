1543c56072c338833985d7d50f7307c94cb095d7
ekaanugrahadii <49528537+ekaanugrahadii@users.noreply.github.com>
2019-04-21 19:55:40 +0700

    Add files via upload

---
+++ 5180711010-TUGASALGO II/5180711010-tugasalgoII.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace::std;
+
+int main(){
+
+ int angka,g,hasil,i;
+   cout<<"masukan bilangan = ";cin>>hasil;
+   cout<<"masukan pangkat = ";cin>>g;
+
+    if(g==0) cout<<"hasil1=1"<<endl;
+       else{
+          angka=hasil;
+        for(i=1;i<g;i++){
+         hasil=hasil+(hasil,g-1);
+           }
+            }
+            cout<<"X pangkat Y = "<<hasil;
+
+}
+
